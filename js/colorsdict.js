a73b4a1da99400a4530482e2891fdb0d2c9e74e6
Suman K Jana <suman.jana@gmail.com>
2018-07-02 20:00:54 -0500

    code clean up and added audio instruction

---
+++ js/colorsdict.js
@@ -0,0 +1,54 @@
+let colorsdict = {
+    English: {
+        'Red': 'Red',
+        'Yellow': 'Yellow',
+        'Blue': 'Blue',
+        'Orange': 'Orange',
+        'Green': 'Green',
+        'Purple': 'Purple',
+        'Pink': 'Pink',
+        'Brown': 'Brown',
+        'Black': 'Black',
+        'White': 'White',
+        'Grey': 'Grey'
+    },
+    Bengali: {
+        'Red': 'লাল',
+        'Yellow': 'হলুদ',
+        'Blue': 'নীল',
+        'Orange': 'কমলা',
+        'Green': 'সবুজ',
+        'Purple': 'বেগুনী',
+        'Pink': 'গোলাপী',
+        'Brown': 'বাদামী',
+        'Black': 'কালো',
+        'White': 'সাদা',
+        'Grey': 'ধূসর'
+    },
+    Hindi: {
+        'Red': 'लाल',
+        'Yellow': 'पीला',
+        'Blue': 'नीला',
+        'Orange': 'नारंगी',
+        'Green': 'हरा',
+        'Purple': 'बैंगनी',
+        'Pink': 'गुलाबी',
+        'Brown': 'भूरा',
+        'Black': 'काली',
+        'White': 'सफेद',
+        'Grey': 'धूसर'
+    },
+    Spanish: {
+        'Red': 'Rojo',
+        'Yellow': 'Amarillo',
+        'Blue': 'Azul',
+        'Orange': 'Naranja',
+        'Green': 'Verde',
+        'Purple': 'Púrpura',
+        'Pink': 'Rosado',
+        'Brown': 'marrón',
+        'Black': 'Negro',
+        'White': 'Blanco',
+        'Grey': 'Gris'
+    }
+}
